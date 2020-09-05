94ba09ab7f6694088d4e909c8367e9a4342273f7
firaskahlout <firaskahlout@outlook.com>
2020-09-05 16:30:54 +0300

    Prepeared files

---
+++ MapApp/Flows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Firas AlKahlout on 9/5/20.
+//  Copyright © 2020 Firas Alkahlout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
